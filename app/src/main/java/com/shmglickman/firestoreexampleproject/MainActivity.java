9b5542443f59ca94990beca41c72c1dea4971822
Shmulik-G <Glickmanso@gmail.com>
2019-04-04 19:54:21 +0300

    Add addSnapshotListener method

--- app/src/main/java/com/shmglickman/firestoreexampleproject/MainActivity.java
+++ app/src/main/java/com/shmglickman/firestoreexampleproject/MainActivity.java
@@ -13,10 +13,15 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.ListenerRegistration;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.annotation.Nullable;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -34,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
     // Or Can Write This
     // private DocumentReference noteRef = db.collection("Notebook").document("My First Note");
 
+//    private ListenerRegistration noteListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,6 +50,35 @@ public class MainActivity extends AppCompatActivity {
         editTextDescription = findViewById(R.id.edit_text_description);
         textViewData = findViewById(R.id.text_view_data);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        noteRef.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+//        noteListener = noteRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                if (e != null) {
+                    Toast.makeText(MainActivity.this, "Error while loading",Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, e.toString());
+                    return;
+                }
+
+                if (documentSnapshot.exists()) {
+                    String title = documentSnapshot.getString(KEY_TITLE);
+                    String description = documentSnapshot.getString(KEY_DESCRIPTION);
+
+                    textViewData.setText("Title: " + title + "\n" + "Description" + description);
+                }
+            }
+        });
+    }
+
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        noteListener.remove();
+//    }
 
     public void saveNote(View v) {
         String title = editTextTitle.getText().toString();
